69ba313201175c6fccf6a30cd10c119963186175
Wesley Kempa <Wesley.Kempa@liferay.com>
2023-02-25 15:16:07 -0500

    Initial Check-in - Proven to work with extension and api key

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [questionInput, setQuestionInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question: questionInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setQuestionInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/askray.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/askray.png" className={styles.icon} />
+        <h3>How can we help?</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="question"
+            placeholder="Enter a question"
+            value={questionInput}
+            onChange={(e) => setQuestionInput(e.target.value)}
+          />
+          <input type="submit" value="Ask RayChat" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
